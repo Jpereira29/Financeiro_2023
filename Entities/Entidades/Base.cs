8fd2392bd586d5866a2f664e63850e1473733fe2
Jpereira29 <jorgepereira29ele@gmail.com>
2023-05-24 21:12:53 -0300

    add entities

---
+++ Entities/Entidades/Base.cs
@@ -0,0 +1,11 @@
+﻿using Entites.Notificacoes;
+
+namespace Entites.Entidades
+{
+    public class Base : Notifica
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ Entities/Entidades/SistemaFinanceiro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entites.Entidades
+{
+    public class SistemaFinanceiro : Base
+    {
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+        public int DiaFechamento { get; set; }
+        public bool GerarCopiaDespesa { get; set; }
+        public int MesCopia { get; set; }
+        public int AnoCopia { get; set; }
+    }
+}
