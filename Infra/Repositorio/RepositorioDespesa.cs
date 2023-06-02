4447554a8d55a2d13fad4baf806bd5c6f04ea7e0
Jpereira29 <jorgepereira29ele@gmail.com>
2023-06-02 16:50:48 -0300

    Customize methods

---
+++ Infra/Repositorio/RepositorioDespesa.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces.Generics;
+using Domain.Interfaces.IDespesa;
+using Entites.Entidades;
+using Infra.Repositorio.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioDespesa : RepositoryGenerics<Despesa>, InterfaceDespesa
+    {
+        public Task<IList<Despesa>> ListaDespesaUsuario(string emailUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Despesa>> ListaDespesaUsuarioNaoPagasAnteriores(string emailUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,11 @@
 using Domain.Interfaces.Generics;
+using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Interfaces.IUsuarioSistemaFinanceiro;
 using Entites.Entidades;
 using Infra.Configuracao;
+using Infra.Repositorio;
 using Infra.Repositorio.Generics;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +28,12 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 
 // INTERFACE E REPOSITORIO
 builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositoryGenerics<>));
+builder.Services.AddSingleton<InterfaceCategoria, RepositorioCategoria>();
+builder.Services.AddSingleton<InterfaceDespesa, RepositorioDespesa>();
+builder.Services.AddSingleton<InterfaceSistemaFinanceiro, RepositorioSistemaFinanceiro>();
+builder.Services.AddSingleton<InterfaceUsuarioSistemaFinanceiro, RepositorioUsuarioSistemaFinanceiro>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
